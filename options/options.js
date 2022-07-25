c483ce103ccb40b08d22099150ec810b76834090
Crowdbotics <team@crowdbotics.com>
2022-07-25 01:30:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "twmmg_for_artist_36394",
+  url: "https://twmmg_for_artist_36394.botics.co",
+  api: "https://twmmg_for_artist_36394.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
