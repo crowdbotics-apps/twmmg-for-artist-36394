ee1ff0631f2ba9ae3e3417354c9bc96aa03a26be
Crowdbotics <team@crowdbotics.com>
2022-07-25 01:36:19 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "twmmg_for_artist_36394",
   url: "https://twmmg_for_artist_36394.botics.co",
   api: "https://twmmg_for_artist_36394.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  undefined: {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Iniciar sesion",
+    SignUpNavText: "Registro",
+    SignInButtonText: "Iniciar sesion",
+    SignUpButtonText: "Registrarme"
   }
 }
